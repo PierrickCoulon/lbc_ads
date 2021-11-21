163c8712f7d6f11653281e0210a8fd272fb8153c
Pierrick <pierrick.coulon@safeware.fr>
2021-11-21 15:08:52 +0100

    feat(xcode): Setup iOS Project

---
+++ lbc_ads/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  lbc_ads
+//
+//  Created by Pierrick Coulon - Safeware on 21/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
